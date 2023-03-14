62a0b0e277e5281feb86a734190b1a9b5ead67ab
phithithuhuong <thu872315@gmail.com>
2023-03-14 09:41:47 +0700

    th2: class components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import AddComponents from "./components/AddComponents";
+
+function App() {
+  return (
+<AddComponents firstNumber={12} secondNumber={2}/>
+  );
+}
+
+export default App;
